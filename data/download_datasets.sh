22c6b0abd19baf1ad7593fb4e109ae4a5eaa12ab
Pierre-Nick Durette <pndurette@gmail.com>
2020-10-03 23:21:37 -0400

    Clean up download_datasets.sh

--- data/download_datasets.sh
+++ data/download_datasets.sh
@@ -1,20 +1,12 @@
 #! /bin/bash
 
-# The following datasets are expected:
-
-# * `cities1000.tsv`, cities with > 1000 pop. (GeoNames):
-#   * https://download.geonames.org/export/dump/cities1000.zip
-#   * Unzipped and renamed to `.tsv`
-# * `countryInfo.tsv`, countries (GeoNames):
-#   * https://download.geonames.org/export/dump/countryInfo.txt
-#   * Unzipped and renamed to `.tsv`
-# * `admin1CodesASCII.txt`, states and provinces (admin1) (GeoNames)
-#   * https://download.geonames.org/export/dump/admin1CodesASCII.txt
-
 # cities
 curl https://download.geonames.org/export/dump/cities1000.zip -o cities1000.zip
 unzip cities1000.zip
 rm cities1000.zip
 
+# countries
 curl https://download.geonames.org/export/dump/countryInfo.txt -o countryInfo.txt
+
+# admin1
 curl https://download.geonames.org/export/dump/admin1CodesASCII.txt -o admin1CodesASCII.txt
